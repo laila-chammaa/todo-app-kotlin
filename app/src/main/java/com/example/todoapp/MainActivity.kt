814aaa25a2952bf1b7c5cb58c4ace272343d4662
Laila Chamma'a <laylachammaa@gmail.com>
2021-01-03 17:06:56 -0500

    adding firebase firestore to save data, still need to work on retrieving and deleting

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,19 +2,34 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var todoAdapter : TodoAdapter
 
+    private val todosCollectionReference = Firebase.firestore.collection("todos")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: change to show previously added items when you implement the database
+        //if there is previous data, it will show that in the todoAdapter, otherwise, it'll show an empty list
         todoAdapter = TodoAdapter(mutableListOf())
+        //TODO: get this to work!!
+        //retrieveTodosDatabase()
+        //Log.d("In main activity", "pls be after, running from ${Thread.currentThread().name}")
 
         rvTodoItems.adapter = todoAdapter
         rvTodoItems.layoutManager = LinearLayoutManager(this)
@@ -26,11 +41,51 @@ class MainActivity : AppCompatActivity() {
                 val todo = Todo(todoTitle)
                 todoAdapter.addTodo(todo)
                 etTodoTitle.text.clear()
+                //adding to the database
+                addTodoDatabase(todo)
             }
         }
 
         btnDeleteDone.setOnClickListener {
             todoAdapter.deleteDoneTodos()
+        }
+    }
+
+    private fun addTodoDatabase(todo : Todo) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            todosCollectionReference.add(todo).await()
+            //this code will only run after the add function is finished because of the await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Successfully saved data", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e : Exception) {
+            //switching to the main context to show the error in a toast
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun retrieveTodosDatabase() = CoroutineScope(Dispatchers.Main).launch {
+        try {
+            Log.d("In main activity", "running from ${Thread.currentThread().name}")
+            val querySnapshot = todosCollectionReference.get().await()
+            //this code will only run after the add function is finished because of the await()
+            val todoList = mutableListOf<Todo>()
+            for (document in querySnapshot.documents) {
+                val todo = document.toObject(Todo::class.java)
+                if (todo != null) {
+                    todoList.add(todo)
+                }
+            }
+            withContext(Dispatchers.Main) {
+                todoAdapter = TodoAdapter(todoList)
+            }
+        } catch (e : Exception) {
+            //switching to the main context to show the error in a toast
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/todoapp/Todo.kt
+++ app/src/main/java/com/example/todoapp/Todo.kt
@@ -1,6 +1,6 @@
 package com.example.todoapp
 
 data class Todo (
-    val title: String,
+    val title: String = "",
     var isChecked: Boolean = false
 )
